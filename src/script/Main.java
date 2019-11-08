92e9dd20632f6b5528dc0dcf36f8e21586818c20
milleja1 <bllitzer@yahoo.com>
2019-11-08 00:57:30 -0500

    Added Nature Spirit... finally!

--- src/script/Main.java
+++ src/script/Main.java
@@ -1,9 +1,6 @@
 package script;
 
-import org.rspeer.runetek.api.Game;
 import org.rspeer.runetek.api.commons.StopWatch;
-import org.rspeer.runetek.api.movement.position.Position;
-import org.rspeer.runetek.api.scene.Projection;
 import org.rspeer.runetek.event.listeners.RenderListener;
 import org.rspeer.runetek.event.types.RenderEvent;
 import org.rspeer.script.ScriptMeta;
@@ -21,7 +18,6 @@ import script.tasks.fungus.Fungus;
 import script.tasks.training.magic.TrainTo13;
 import script.tasks.training.prayer.TrainTo50;
 
-import java.awt.*;
 import java.util.HashMap;
 
 @ScriptMeta(developer = "Streagrem", name = "LOL", desc = "LOL")
@@ -29,43 +25,46 @@ public class Main extends TaskScript implements RenderListener {
 
     private ScriptPaint paint;
     private StopWatch runtime;
+    private HashMap<String, Integer> ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION;
 
     public StopWatch getRuntime() {
         return runtime;
     }
 
-    private static final String[] ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION = new String[]{
-            "Lumbridge teleport",
-            "Staff of air",
-            "Staff of fire",
-            "Amulet of glory(6)",
-            "Ring of wealth (5)",
-            "Air rune",
-            "Mind rune",
-            "Water rune",
-            "Fire rune",
-            "Earth rune",
-            "Tuna",
-            "Stamina potion(4)",
-            "Cheese",
-            "Leather gloves",
-            "Falador teleport",
-            "Games necklace(8)",
-            "Rope",
-            "Adamant scimitar",
-            "Ring of recoil",
-            "Bucket",
-            "Rune essence",
-            "Varrock teleport",
-            "Silver sickle",
-            "Dragon bones",
-            "Burning amulet(5)"
-    };
+    private void populateStartingItemsMap() {
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION = new HashMap<>();
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Lumbridge teleport", 10);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Staff of air", 1);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Staff of fire", 1);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Amulet of glory(6)", 5);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Ring of wealth (5)", 2);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Air rune", 1000);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Mind rune", 1000);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Water rune", 200);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Fire rune", 200);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Earth rune", 300);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Tuna", 100);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Stamina potion(4)", 10);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Cheese", 2);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Leather gloves", 1);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Falador teleport", 5);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Games necklace(8)", 1);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Rope", 2);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Adamant scimitar", 1);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Ring of recoil", 1);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Bucket", 1);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Rune essence", 50);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Varrock teleport", 5);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Silver sickle", 1);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Dragon bones", 300);
+        ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION.put("Burning amulet(5)", 5);
+    }
 
     @Override
     public void onStart() {
         runtime = StopWatch.start();
         paint = new ScriptPaint(this);
+        populateStartingItemsMap();
 
         submit(new BuySupplies(ALL_ITEMS_NEEDED_FOR_ACCOUNT_PREPERATION),
                 new GetStartersGold(),
